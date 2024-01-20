5bbb8965d789d261d13417a7def536d23d012f3e
harsh-malik-0803 <75578267+harsh-malik-0803@users.noreply.github.com>
2024-01-20 12:56:17 +0530

    Add files via upload
    enhanced code by modern c++

--- servertest/server.cpp
+++ servertest/server.cpp
@@ -6,135 +6,177 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <thread>
-#include <map>
 #include <vector>
 #include <string>
 #include <queue>
 #include <sstream>
 #include <stdio.h>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <unordered_set>
+#include <future>
+#include <memory>
+
 using namespace std;
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
-// #pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "Mswsock.lib")
    
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "27015"
 
-class semaphore {
-    int s;
+class Client {
 public:
-    semaphore() {
-        s = 1;
-    }
-    void wait() {
-        while (s <= 0);
-        ++s;
-    }
-    void signal() {
-        --s;
-    }
-};
-
-semaphore sem1, sem2;
-
-// client class 
-class client {
-public:
-    SOCKET ClientSocket;
+    SOCKET socket = INVALID_SOCKET;
     string name;
-    client() {
-        ClientSocket = INVALID_SOCKET;
-        name = "";
-    }
-    client(SOCKET s1) {
-        ClientSocket = s1;
-    }
-    client(SOCKET s1, string n) {
-        ClientSocket = s1;
-        name = n;
-    }
-    ~client() {
-        cout << "Removing client " << ClientSocket << endl;
-        // cleanup
-        closesocket(ClientSocket);
-        WSACleanup();
-    }
-};
-
-class queue_type {
-public:
-    string message;
-    client* newclient;
-    queue_type() {
-        message = "";
-        newclient = NULL;
-    }
-    queue_type(string mes, client* c) {
-        message = mes;
-        newclient = c;
-    }
-};
-// making a queue to store messages 
-queue<queue_type> message_q;
-bool server_running = true;
-map< client*, int > clients;
-
-// Define a Lambda Expression for checking message arrived or not 
-auto func2 = [](client* c1) {
-
-    int recvbuflen = DEFAULT_BUFLEN;
-    char recvbuf[DEFAULT_BUFLEN];
-    int iResult = 1;
-
-    while (server_running && iResult >0 ) {
-        int iResult = recv(c1->ClientSocket, recvbuf, recvbuflen, 0);
-        if (iResult > 0 ) {
-
-            ostringstream ss;
-            ss << "SOCKET #" << c1->ClientSocket << ": " << recvbuf << "\r";
-            string strOut = ss.str();
-
-            cout << "client with socket id : " << c1->ClientSocket << " send a message ." << endl;
-            sem1.wait();
-            message_q.push(queue_type(strOut, c1));
-            sem1.signal();
-        }
-        else {
-            cout << "client with socket id : " <<c1->ClientSocket<<" left ." << endl;
-            clients.erase(c1);
-            return;
-        }
-    }
-};
-
-
-class server {
-public:
-    SOCKET ListenSocket;
-    
-    map<int, std::thread> vecofthreads;
     int id;
 
-    server() {
-        ListenSocket = INVALID_SOCKET;
-        id = 0;
-        vecofthreads.clear();
+    Client(SOCKET socket, int id) : socket(socket), id(id) {}
+    Client(SOCKET socket, string name) : socket(socket), name(name) {}
+
+    ~Client() {
+        cout << "Removing Client " << socket << endl;
+        closesocket(socket);
     }
-    bool on_startup() {
+};
+
+// making a queue to store messages 
+
+template<typename T>
+class ConcurrentQueue {
+    queue<T> m_data;
+    mutex mtx;
+    condition_variable cv;
+public:
+    void enqueue(const T& item) {
+        unique_lock<mutex> lock(mtx);
+        m_data.push(item);
+        lock.unlock();
+        cv.notify_one();
+    }
+    T dequeue(){
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [this] { return m_data.size() != 0; });
+        T data = m_data.front();
+        m_data.pop();
+        return data;
+    }
+};
+
+ConcurrentQueue< pair< string, Client* > > messageQueue;
+
+class Server {
+public:
+    SOCKET ListenSocket = INVALID_SOCKET;
+    vector<thread> clientThreads;
+    int clientIdCounter = 0;
+    unordered_set<Client*> connectedClients;
+    // variable for server state
+    atomic<bool> serverRunning{ true };
+
+    Server() {
+        if (initialize() != 0) {
+            cout << "Error in starting the server. Retry starting again!" << endl;
+            exit(1);
+        }
+        else {
+            cout << "Server started successfully!!" << endl;
+        }
+    }
+
+    ~Server() {
+        serverRunning = false;
+        closesocket(ListenSocket);
+
+        // shutdown connected clients
+        for (auto& client : connectedClients) {
+            int iResult = shutdown(client->socket, SD_SEND);
+            closesocket(client->socket);
+            delete client;
+        }
+
+        // clean up client threads
+        for (auto& thread : clientThreads) {
+            if (thread.joinable()) thread.join();
+        }
+
+        connectedClients.clear();
+        clientThreads.clear();
+        WSACleanup();
+    }
+
+    // Handle a new client connection
+    void handleNewClient(SOCKET clientSocket) {
+        cout << "Client with socket id: " << clientSocket << " trying to enter the server." << endl;
+
+        // Send a welcome message to the connected client
+        string welcomeMessage = "Welcome to the Awesome Chat Server!\r";
+        int sendResult = send(clientSocket, welcomeMessage.c_str(), welcomeMessage.size() + 1, 0);
+
+        if (sendResult > 0) {
+            Client* newClient = new Client(clientSocket, clientIdCounter++);
+            ostringstream joinMessage;
+            joinMessage << "SOCKET #" << clientSocket << ": joined the chat";
+            string joinMessageStr = joinMessage.str();
+            messageQueue.enqueue({ joinMessageStr, newClient });
+            cout << "SOCKET #" << clientSocket << ": connected to the server\n";
+
+            connectedClients.insert(newClient);
+            clientThreads.push_back(thread(&Server::checkClientMessages, this, newClient));
+        }
+        else {
+            cout << "client connection failed\n";
+            closesocket(clientSocket);
+        }
+    }
+
+    // accept client connections
+    void acceptClientsLoop() {
+        while (serverRunning) {
+            SOCKET clientSocket = accept(ListenSocket, nullptr, nullptr);
+            if (clientSocket != INVALID_SOCKET) {
+                handleNewClient(clientSocket);
+            }
+        }
+    }
+
+    // check for messages from a specific client
+    void checkClientMessages(Client* client) {
+        int recvBufferSize = DEFAULT_BUFLEN;
+        char recvBuffer[DEFAULT_BUFLEN];
+
+        while (serverRunning) {
+            int receiveResult = recv(client->socket, recvBuffer, recvBufferSize, 0);
+            if (receiveResult > 0) {
+                ostringstream messageStream;
+                messageStream << "SOCKET #" << client->socket << ": " << recvBuffer << "\r";
+                string messageStr = messageStream.str();
+                cout << "Client with socket id: " << client->socket << " sent a message." << endl;
+                messageQueue.enqueue({ messageStr, client });
+            }
+            else {
+                cout << "Client with socket id: " << client->socket << " left." << endl;
+
+                // Clean up disconnected client
+                connectedClients.erase(client);
+                delete client;
+                return;
+            }
+        }
+    }
+private:
+   
+    int initialize() {
         WSADATA wsaData;
-        int iResult;
-
-        struct addrinfo* result = NULL;
-        struct addrinfo hints;
-
-        int iSendResult;
-
-        // Initialize Winsock
-        iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-        if (iResult != 0) {
-            printf("WSAStartup failed with error: %d\n", iResult);
+        int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (result != 0) {
+            cout << "WSAStartup failed with error: " << result << endl;
             return 1;
         }
+
+        struct addrinfo* resultInfo = nullptr;
+        struct addrinfo hints;
 
         ZeroMemory(&hints, sizeof(hints));
         hints.ai_family = AF_INET;
@@ -142,151 +184,80 @@ public:
         hints.ai_protocol = IPPROTO_TCP;
         hints.ai_flags = AI_PASSIVE;
 
-        // Resolve the server address and port
-        iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-        if (iResult != 0) {
-            printf("getaddrinfo failed with error: %d\n", iResult);
+        result = getaddrinfo(nullptr, DEFAULT_PORT, &hints, &resultInfo);
+        if (result != 0) {
+            cout << "getaddrinfo failed with error: " << result << endl;
             WSACleanup();
             return 1;
         }
 
-        // Create a SOCKET for connecting to server
-        ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+        ListenSocket = socket(resultInfo->ai_family, resultInfo->ai_socktype, resultInfo->ai_protocol);
         if (ListenSocket == INVALID_SOCKET) {
-            printf("socket failed with error: %ld\n", WSAGetLastError());
-            freeaddrinfo(result);
+            cout << "socket failed with error: " << WSAGetLastError() << endl;
+            freeaddrinfo(resultInfo);
             WSACleanup();
             return 1;
         }
 
-        // Setup the TCP listening socket
-        iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-        if (iResult == SOCKET_ERROR) {
-            printf("bind failed with error: %d\n", WSAGetLastError());
-            freeaddrinfo(result);
+        result = ::bind(ListenSocket, resultInfo->ai_addr, static_cast<int>(resultInfo->ai_addrlen));
+        if (result == SOCKET_ERROR) {
+            cout << "bind failed with error: " << WSAGetLastError() << endl;
+            freeaddrinfo(resultInfo);
             closesocket(ListenSocket);
             WSACleanup();
             return 1;
         }
 
-        freeaddrinfo(result);
-
-        iResult = listen(ListenSocket, SOMAXCONN);
-        if (iResult == SOCKET_ERROR) {
-            printf("listen failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(resultInfo);
+        result = listen(ListenSocket, SOMAXCONN);
+        if (result == SOCKET_ERROR) {
+            cout << "listen failed with error: " << WSAGetLastError() << endl;
             closesocket(ListenSocket);
             WSACleanup();
             return 1;
         }
+
         return 0;
-    }
-
-    // it runs when a new client comes 
-    void on_newClient(SOCKET clientsocket) {
-        cout << "client with socket id : " << clientsocket << " trying to enter into server . " << endl;
-
-        // Send a welcome message to the connected client
-        string message = "Welcome to the Awesome Chat Server!\r";
-        int iResult = send(clientsocket, message.c_str(), message.size() + 1, 0);
-
-        if (iResult > 0) {
-            
-            client* c1 = new client(clientsocket);
-            ostringstream ss;
-            ss << "SOCKET #" << clientsocket << ": joined the chat" << "\r";
-            string strOut = ss.str();
-            message_q.push(queue_type(strOut, c1));
-            cout << "SOCKET #" << clientsocket << ": connected to  server \n";
-
-            clients[c1] = id;
-            vecofthreads[id] = std::thread(func2, c1);
-            ++id;
-        }
-        else {
-            closesocket(clientsocket);
-            WSACleanup();
-        }
-    }
-
-    ~server() {
-        server_running = false;
-        
-        closesocket(ListenSocket);
-        for (auto it : clients) {
-
-            int iResult = shutdown(it.first->ClientSocket, SD_SEND);
-            // cleanup
-            closesocket(it.first->ClientSocket);
-            WSACleanup();
-            delete (it.first);
-
-            vecofthreads[it.second].join();
-            vecofthreads.erase(it.second);
-            clients.erase(it.first);
-            
-        }
-        clients.clear();
-        vecofthreads.clear();
     }
 };
 
+// function to send messages from the messageQueue to clients
+void sendMessage(Server* server) {
+    while (server->serverRunning) {
+        pair<string, Client*> messageItem = messageQueue.dequeue();
 
-void fun1(server* s1) {
-    while (server_running) {
-
-        // Accept a client socket
-        SOCKET clientsocket = accept(s1->ListenSocket, NULL, NULL);
-        if (clientsocket != INVALID_SOCKET) {
-            s1->on_newClient(clientsocket);
-        }
-    }
-}
-
-void fun3(server* s1) {
-    while (server_running) {
-
-        int len = message_q.size();
-
-        if (len > 0) {
-
-            auto front = message_q.front();
-            message_q.pop();
-
-            for (auto it : clients) {
-                if (front.newclient != it.first) {
-                    int iResult = send(it.first->ClientSocket, front.message.c_str(), front.message.length() + 1, 0);
-                    if (iResult == SOCKET_ERROR) {
+        vector<future<void>> futures;
+        for (auto& client : server->connectedClients) {
+            auto future = async(launch::async, [server , &client, &messageItem] {
+                if (client != messageItem.second) {
+                    int sendResult = send(client->socket, messageItem.first.c_str(), messageItem.first.length() + 1, 0);
+                    if (sendResult == SOCKET_ERROR) {
                         cout << "send failed with error: " << WSAGetLastError() << endl;
 
-                        clients.erase(it.first);
-                        delete it.first ;
+                        server->connectedClients.erase(client);
+                        delete client;
                     }
                 }
-            }
-            cout << "message has been sent to all clients " << endl;
+                });
+
+            futures.push_back(move(future));
         }
+
+        for_each(futures.begin(), futures.end(), [](future<void>& future) { future.wait(); });
+        cout << "Message has been sent to all clients." << endl;
     }
 }
 
 int __cdecl main(void)
 {
-    server* s1 = new server();
-    if (s1->on_startup() == 0) {
-        cout << "Server Started Successfully " << endl;
-    }
-    else {
-        cout << "Error in starting server . \nRetry starting again !!" << endl;
-        return 0;
-    }
+    unique_ptr<Server> server = make_unique<Server>();
+    // thread for accepting client connections
+    thread acceptClientThread(&Server::acceptClientsLoop, server.get());
 
-    // thread th1 for accepting connection to clients 
-    std::thread th1(fun1, s1);
-    // thread2 for sending messages from message queue to clients 
-    std::thread th2(fun3, s1);
+    // thread for sending messages to clients
+    thread sendMessageThread(sendMessage, server.get());
 
-    if (th1.joinable()) th1.join();
-    if (th2.joinable()) th2.join();
-
-    delete s1;
+    if (acceptClientThread.joinable()) acceptClientThread.join();
+    if (sendMessageThread.joinable()) sendMessageThread.join();
     return 0;
 }
